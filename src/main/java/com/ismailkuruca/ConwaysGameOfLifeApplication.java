b373ef42202e34009e817b992572c3fe29ec2076
ismail kuruca <ismailkuruca@gmail.com>
2017-04-18 00:22:54 +0300

    spring boot init

---
+++ src/main/java/com/ismailkuruca/ConwaysGameOfLifeApplication.java
@@ -0,0 +1,12 @@
+package com.ismailkuruca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConwaysGameOfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConwaysGameOfLifeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ismailkuruca/ConwaysGameOfLifeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ismailkuruca;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConwaysGameOfLifeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
